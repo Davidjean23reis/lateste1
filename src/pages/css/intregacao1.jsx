0abbb193e97663cef17d2f4001809b742222d489
David <david.jean.reis23@gmail.com>
2023-11-10 16:34:58 -0300

    Integração com CSS

---
+++ src/pages/css/intregacao1.jsx
@@ -0,0 +1,12 @@
+
+export default function intregacao() {
+    return (
+        <div>
+            <>
+                <div className="vermelha">Texto #01</div>
+                <div className="azul">Texto #02</div>
+                <div className="branca">Text #03</div>
+            </>
+        </div>
+    )
+}
