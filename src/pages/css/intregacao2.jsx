fb914bd09dc50b3c773c46c3378b5bb15084e2b8
David <david.jean.reis23@gmail.com>
2023-11-10 18:26:58 -0300

    Integração com CSS 02

---
+++ src/pages/css/intregacao2.jsx
@@ -0,0 +1,11 @@
+import styles from "./intregacao2.module.css"
+
+export default function intregacao2(){
+    return (
+        <div id={styles.intregacao2}>
+            <div className={styles.vermelha}>Texto #01</div>
+            <div className={styles.azul}>Texto #02</div>
+            <div className={styles.branca}>Text #03</div>
+    </div>
+    )
+}
