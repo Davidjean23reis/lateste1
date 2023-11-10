ce09b9e8f7da73c3a0981593a1c4da0ff7a24594
David <david.jean.reis23@gmail.com>
2023-11-10 11:36:41 -0300

    Exercícios jsx

--- src/pages/sengundo.jsx
+++ src/pages/sengundo.jsx
@@ -1,1 +1,21 @@
-weweddsdds
+export default function jsx() {
+    const subtitulo = "Estou no Javascript"
+    return (
+        <div>
+            <h1>Intregação JS e JSX</h1>
+            <h2>{subtitulo}</h2>
+            <h2> {3 * 3} </h2>
+            <h4> {Math.max(19, 39)}</h4>
+            <h5>{entre(30, 1, 10)}</h5>
+
+        </div>
+    )
+}
+
+function entre(valor, min, max) {
+    if (valor > min && valor < max) {
+        return "sim"
+    } else {
+        return "não"
+    }
+}
